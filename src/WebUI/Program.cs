900f0758007a8c8eb9b97868c8135a8c743669bb
Ada Orajiaku <adaorajiaku@gmail.com>
2023-07-02 19:36:29 +0100

    --fixing prod db migrate

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -25,6 +25,17 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+
+        using (var scope = app.Services.CreateScope())
+    {
+        var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+        await initialiser.InitialiseAsync();
+        await initialiser.SeedAsync();
+    }
+
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
