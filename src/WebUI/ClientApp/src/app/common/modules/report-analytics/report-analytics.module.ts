f509d4cb31c3c9d1a116c56471f64888c2414889
Ada Orajiaku <adaorajiaku@gmail.com>
2023-11-29 00:56:55 +0000

    WIP added language translations

--- src/WebUI/ClientApp/src/app/common/modules/report-analytics/report-analytics.module.ts
+++ src/WebUI/ClientApp/src/app/common/modules/report-analytics/report-analytics.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { ReportAnalyticsComponent } from './report-analytics.component';
 import { SharedModule } from 'app/shared/shared.module';
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
     declarations: [ReportAnalyticsComponent],
-    imports: [SharedModule, NgApexchartsModule],
+    imports: [SharedModule, NgApexchartsModule, TranslocoModule],
     exports: [ReportAnalyticsComponent],
 })
 export class ReportAnalyticsModule {}
